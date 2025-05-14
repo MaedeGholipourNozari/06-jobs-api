254bea0fab75e40464c8dc5e9a9244abce724494
Maede Gholipour <maedegholipour@gmail.com>
2025-05-14 14:37:23 -0400

    First Experience Api for my application

--- app.js
+++ app.js
@@ -1,27 +1,32 @@
 require('dotenv').config();
 require('express-async-errors');
+
 const express = require('express');
 const app = express();
 
+const connectDB = require('./db/connect');
+const authenticateUser = require('./middleware/authentication');
+// routers
+const authRouter = require('./routes/auth');
+const experiencesRouter = require('./routes/experiences');
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.use(express.json());
-// extra packages
-
+ 
 // routes
-app.get('/', (req, res) => {
-  res.send('jobs api');
-});
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/experiences', authenticateUser, experiencesRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const start = async () => {
-  try {
+  try { 
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
---
+++ models/Experience.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+const ExperienceSchema = new mongoose.Schema(
+  {
+    company: {
+      type: String,
+      required: [true, 'Please provide company name'],
+      maxlength: 50,
+    },
+    position: {
+      type: String,
+      required: [true, 'Please provide position'],
+      maxlength: 100,
+    },
+    status: {
+      type: String,
+      enum: ['Remote', 'On-site', 'Hybrid'],
+      default: 'Remote', 
+    },
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'Please provide user'],
+    },
+  },
+  { timestamps: true }
+)
+
+module.exports = mongoose.model('Experience', ExperienceSchema)
