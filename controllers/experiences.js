198ee4ef2a13e7666ecf664bcd2fd9140d77708a
Maede Gholipour <maedegholipour@gmail.com>
2025-05-20 20:18:44 -0400

    API 2

--- controllers/experiences.js
+++ controllers/experiences.js
@@ -3,11 +3,23 @@ const { StatusCodes } = require('http-status-codes')
 const { BadRequestError, NotFoundError } = require('../errors')
 
 const getAllExperiences = async (req, res) => {
-  
-  res.send({ msg: 'get all experiences' })
+   const experiences = await Experience.find({ createdBy: req.user.userId }).sort('createdAt')
+  res.status(StatusCodes.OK).json({ experiences, count: experiences.length })
 }
 const getExperience = async (req, res) => {
-  res.send({ msg: 'get single experience' })
+   const {
+    user: { userId },
+    params: { id: experienceId },
+  } = req
+
+  const experience = await Experience.findOne({
+    _id: experienceId,
+    createdBy: userId,
+  })
+  if (!experience) {
+    throw new NotFoundError(`No job with id ${experienceId}`)
+  }
+  res.status(StatusCodes.OK).json({ experience })
 }
 
 const createExperience = async (req, res) => {
@@ -17,11 +29,40 @@ const createExperience = async (req, res) => {
 }
 
 const updateExperience = async (req, res) => {
-  res.send({ msg: 'update experience' })
+  const {
+    body: { company, position },
+    user: { userId },
+    params: { id: experienceId },
+  } = req
+
+  if (company === '' || position === '') {
+    throw new BadRequestError('Company or Position fields cannot be empty')
+  }
+  const experience = await Experience.findByIdAndUpdate(
+    { _id: experienceId, createdBy: userId },
+    req.body,
+    { new: true, runValidators: true }
+  )
+  if (!experience) {
+    throw new NotFoundError(`No experience with id ${experienceId}`)
+  }
+  res.status(StatusCodes.OK).json({ experience })
 }
 
 const deleteExperience = async (req, res) => {
-  res.send({ msg: 'delete experience' })
+  const {
+    user: { userId },
+    params: { id: experienceId },
+  } = req
+
+  const experience = await Experience.findByIdAndRemove({
+    _id: experienceId,
+    createdBy: userId,
+  })
+  if (!experience) {
+    throw new NotFoundError(`No experience with id ${experienceId}`)
+  }
+  res.status(StatusCodes.OK).send()
 }
 
 module.exports = {
